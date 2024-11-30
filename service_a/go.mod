5f853f1590cafcfd2612d59e6c09ef92709c0baa
Wesley Santos <wesley.massine@gmail.com>
2024-11-30 22:47:05 +0000

    new version 2.0 refactoring

--- service_a/go.mod
+++ service_a/go.mod
@@ -3,6 +3,10 @@ module github.com/4lexRossi/service-a-weather-opel-go
 go 1.22.5
 
 require (
+	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/golang/mock v1.6.0
+	github.com/hashicorp/go-retryablehttp v0.7.7
+	github.com/stretchr/testify v1.10.0
 	go.opentelemetry.io/otel v1.32.0
 	go.opentelemetry.io/otel/exporters/zipkin v1.32.0
 	go.opentelemetry.io/otel/sdk v1.32.0
@@ -10,10 +14,30 @@ require (
 )
 
 require (
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gofiber/fiber v1.14.6
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/openzipkin/zipkin-go v0.4.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.opentelemetry.io/otel/metric v1.32.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- service_a/main.go
+++ service_a/main.go
@@ -1,128 +1,103 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"regexp"
 
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/zipkin"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-	tracers "go.opentelemetry.io/otel/trace"
+	"github.com/4lexRossi/service-a-weather-opel-go/utils"
+	"github.com/gofiber/fiber/v2"
+	"github.com/hashicorp/go-retryablehttp"
+	"go.opentelemetry.io/otel/trace"
 )
 
-var tracer tracers.Tracer
+var tracer trace.Tracer
 
-type CepRequest struct {
-	Cep string `json:"cep"`
+type PostcodeRequest struct {
+	CEP string `json:"cep"`
 }
 
 func main() {
-	// Update Zipkin endpoint to explicitly use IPv4
-	const zipkinEndpoint = "http://zipkin:9411/api/v2/spans"
-	exporter, err := zipkin.New(zipkinEndpoint)
-	if err != nil {
-		log.Fatalf("Failed to configure Zipkin exporter: %v", err)
-	}
+	// Initialize OTEL tracing
+	tp := utils.InitTracer("service-a")
 
-	tp := trace.NewTracerProvider(
-		trace.WithBatcher(exporter),
-		trace.WithResource(resource.NewWithAttributes("service-a",
-			attribute.String("service.name", "servico-a"))),
-	)
-	otel.SetTracerProvider(tp)
+	// Initialize Fiber app
+	app := fiber.New()
 
-	tracer = tp.Tracer("servico-a-tracing")
+	tracer = tp.Tracer("service-a-tracing")
 
-	http.HandleFunc("/cep", handleCepRequest)
+	// Define routes
+	app.Post("/cep", validatePostcode)
 
-	log.Println("Servidor A iniciado na porta 8081...")
-	err = http.ListenAndServe(":8081", nil)
-	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
-	}
+	// Start the server
+	log.Println("Service A running on http://localhost:8081")
+	log.Fatal(app.Listen(":8081"))
 }
 
-func handleCepRequest(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var req CepRequest
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Erro ao ler corpo da requisição", http.StatusInternalServerError)
-		return
-	}
-
-	err = json.Unmarshal(body, &req)
-	if err != nil {
-		http.Error(w, "Formato inválido", http.StatusBadRequest)
-		return
-	}
-
-	if !isValidCEP(req.Cep) {
-		http.Error(w, "invalid zipcode", http.StatusUnprocessableEntity)
-		return
-	}
-
-	ctx, span := tracer.Start(r.Context(), "Chamando o Serviço B")
+func validatePostcode(c *fiber.Ctx) error {
+	// Start tracing span
+	ctx, span := tracer.Start(c.Context(), "validatePostcode")
 	defer span.End()
 
-	cityInfo, err := callServicoB(ctx, req.Cep)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	// Parse request body
+	var req PostcodeRequest
+	if err := c.BodyParser(&req); err != nil {
+		return c.Status(http.StatusUnprocessableEntity).JSON(fiber.Map{"message": "invalid zipcode"})
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(cityInfo)
-}
-
-func isValidCEP(cep string) bool {
-	re := regexp.MustCompile(`^\d{8}$`)
-	return re.MatchString(cep)
-}
-
-func callServicoB(ctx context.Context, cep string) (map[string]interface{}, error) {
-	ctx, span := tracer.Start(ctx, "Chamada ao Serviço B")
-	defer span.End()
-
-	requestBody, err := json.Marshal(map[string]string{"cep": cep})
-	if err != nil {
-		return nil, fmt.Errorf("erro ao montar o corpo da requisição para o Serviço B")
+	// Validate postcode
+	if !isValidPostcode(req.CEP) {
+		return c.Status(http.StatusUnprocessableEntity).JSON(fiber.Map{"message": "invalid zipcode"})
 	}
 
-	resp, err := http.Post("http://service-b:8082/weather", "application/json", bytes.NewBuffer(requestBody))
+	// Forward to Service B
+	forwardedResponse, err := forwardToServiceB(ctx, req.CEP)
 	if err != nil {
-		return nil, fmt.Errorf("erro ao chamar o Serviço B: %v", err)
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"message": "error forwarding request"})
 	}
-	defer resp.Body.Close()
+	defer forwardedResponse.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(resp.Body)
-		return nil, fmt.Errorf("Serviço B retornou erro: %s", string(body))
+	// Read the response body
+	bodyBytes, err := io.ReadAll(forwardedResponse.Body)
+	if err != nil {
+		log.Printf("Error reading response body: %v", err)
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"message": "error reading response"})
 	}
 
 	var cityInfo map[string]interface{}
-	body, err := io.ReadAll(resp.Body)
+	err = json.Unmarshal(bodyBytes, &cityInfo)
 	if err != nil {
-		return nil, fmt.Errorf("erro ao ler resposta do Serviço B")
+		return c.Status(http.StatusBadRequest).JSON("erro ao decodificar resposta do Serviço B")
 	}
 
-	err = json.Unmarshal(body, &cityInfo)
+	// Respond with Service B's response
+	return c.Status(forwardedResponse.StatusCode).JSON(cityInfo)
+}
+
+func forwardToServiceB(ctx context.Context, cep string) (*http.Response, error) {
+	client := retryablehttp.NewClient()
+	client.RetryMax = 3
+
+	requestBody, _ := json.Marshal(PostcodeRequest{CEP: cep})
+	req, err := retryablehttp.NewRequest("POST", "http://localhost:8082/weather", requestBody)
 	if err != nil {
-		return nil, fmt.Errorf("erro ao decodificar resposta do Serviço B")
+		return nil, err
 	}
 
-	return cityInfo, nil
+	req.Header.Set("Content-Type", "application/json")
+	return client.Do(req)
+}
+
+func isValidPostcode(cep string) bool {
+	if len(cep) != 8 {
+		return false
+	}
+	for _, char := range cep {
+		if char < '0' || char > '9' {
+			return false
+		}
+	}
+	return true
 }
